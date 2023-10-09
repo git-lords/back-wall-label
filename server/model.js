7734cbdefc7028f5c7e748ce2b9ead0cc0f22ae5
Priscila Schleske <plasch7@gmail.com>
2023-10-09 15:48:42 -0600

    model in progress

---
+++ server/model.js
@@ -0,0 +1,168 @@
+import { DataTypes, Model } from "sequelize";
+import url from 'url';
+import connectToDb from './db.js';
+import util from 'util';
+
+const db = await connectToDb('postgresql:///bw-db');
+
+class User extends Model {
+    [util.inspect.custom]() {
+        return this.toJSON();
+    }
+}
+
+User.init(
+    {
+        userId: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        userName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING(500),
+        },
+        adminStatus: {
+            type: DataTypes.BOOLEAN,
+        },
+        bandStatus: {
+            type: DataTypes.BOOLEAN,
+        },
+
+    },
+    {
+        modelName: 'user',
+        sequelize: db
+    }
+)
+
+class Product extends Model {
+    [util.inspect.custom]() {
+        return this.toJSON();
+    }
+}
+
+Product.init(
+    {
+        productId: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        productName: {
+            type: DataTypes.STRING,
+        },
+        category: {
+            type: DataTypes.STRING,
+        },
+        price: {
+            type: DataTypes.INTEGER,
+        },
+        description: {
+            type: DataTypes.STRING,
+        },
+        // imgUrl: {
+        //     type: DataTypes.STRING,
+        // },
+    },
+    {
+        modelName: 'product',
+        sequelize: db
+    }
+)
+
+class Band extends Model {
+    [util.inspect.custom]() {
+        return this.toJSON();
+    }
+}
+
+Band.init(
+    {
+        bandId: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        bandName: {
+            type: DataTypes.STRING,
+        },
+        bio: {
+            type: DataTypes.STRING,
+        }
+
+    },
+    {
+        modelName: 'band',
+        sequelize: db
+    }
+)
+
+class Event extends Model {
+    [util.inspect.custom]() {
+        return this.toJSON();
+    }
+}
+
+Event.init(
+    {
+        eventId: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        date: {
+            type: DataTypes.DATE,
+        },
+        location: {
+            type: DataTypes.STRING,
+        },
+        time: {
+            type: DataTypes.TIME
+        },
+        description: {
+            type: DataTypes.STRING,
+        },
+        bands: {
+            type: DataTypes.STRING,
+        },
+        links: {
+            type: DataTypes.STRING,
+        },
+        isSoldOut: {
+            type: DataTypes.BOOLEAN,
+        },
+
+    },
+    {
+        modelName: 'event',
+        sequelize: db
+    }
+)
+
+class Order extends Model {
+    [util.inspect.custom]() {
+        return this.toJSON();
+    }
+}
+
+Order.init(
+    {
+        userId: {
+            type: DataTypes.INTEGER
+        }
+    },
+    {
+        modelName: 'order';
+        sequelize: db
+    }
+)
