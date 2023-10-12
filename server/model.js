ba1984490e3124d741330e81c5bd6af1e8616f1e
Priscila Schleske <plasch7@gmail.com>
2023-10-12 13:13:34 -0600

    adds priceId column to product model

--- server/model.js
+++ server/model.js
@@ -6,208 +6,208 @@ import util from 'util';
 const db = await connectToDb('postgresql:///backwalldb');
 
 class User extends Model {
-    [util.inspect.custom]() {
-        return this.toJSON();
-    }
+  [util.inspect.custom]() {
+    return this.toJSON();
+  }
 }
 
 User.init(
-    {
-        userId: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        userName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-        },
-        password: {
-            type: DataTypes.STRING(500),
-        },
-        adminStatus: {
-            type: DataTypes.BOOLEAN,
-        },
-        bandStatus: {
-            type: DataTypes.BOOLEAN,
-        },
-
+  {
+    userId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        modelName: 'user',
-        sequelize: db
-    }
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING(500),
+    },
+    adminStatus: {
+      type: DataTypes.BOOLEAN,
+    },
+    bandStatus: {
+      type: DataTypes.BOOLEAN,
+    },
+
+  },
+  {
+    modelName: 'user',
+    sequelize: db
+  }
 )
 
 class Product extends Model {
-    [util.inspect.custom]() {
-        return this.toJSON();
-    }
+  [util.inspect.custom]() {
+    return this.toJSON();
+  }
 }
 
 Product.init(
-    {
-        productId: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        productName: {
-            type: DataTypes.STRING,
-        },
-        category: {
-            type: DataTypes.STRING,
-        },
-        price: {
-            type: DataTypes.INTEGER,
-        },
-        description: {
-            type: DataTypes.STRING,
-        },
-        // priceId: {
-        //     type: DataTypes.STRING,
-        // }
-        // imgUrl: {
-        //     type: DataTypes.STRING,
-        // },
+  {
+    productId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        modelName: 'product',
-        sequelize: db
+    productName: {
+      type: DataTypes.STRING,
+    },
+    category: {
+      type: DataTypes.STRING,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    priceId: {
+      type: DataTypes.STRING,
     }
+    // imgUrl: {
+    //     type: DataTypes.STRING,
+    // },
+  },
+  {
+    modelName: 'product',
+    sequelize: db
+  }
 )
 
 class Band extends Model {
-    [util.inspect.custom]() {
-        return this.toJSON();
-    }
+  [util.inspect.custom]() {
+    return this.toJSON();
+  }
 }
 
 Band.init(
-    {
-        bandId: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        bandName: {
-            type: DataTypes.STRING,
-        },
-        bio: {
-            type: DataTypes.STRING,
-        }
-
+  {
+    bandId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        modelName: 'band',
-        sequelize: db
+    bandName: {
+      type: DataTypes.STRING,
+    },
+    bio: {
+      type: DataTypes.STRING,
     }
+
+  },
+  {
+    modelName: 'band',
+    sequelize: db
+  }
 )
 
 class Event extends Model {
-    [util.inspect.custom]() {
-        return this.toJSON();
-    }
+  [util.inspect.custom]() {
+    return this.toJSON();
+  }
 }
 
 Event.init(
-    {
-        eventId: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        date: {
-            type: DataTypes.DATE,
-        },
-        location: {
-            type: DataTypes.STRING,
-        },
-        time: {
-            type: DataTypes.TIME
-        },
-        description: {
-            type: DataTypes.STRING,
-        },
-        bands: {
-            type: DataTypes.STRING,
-        },
-        links: {
-            type: DataTypes.STRING,
-        },
-        isSoldOut: {
-            type: DataTypes.BOOLEAN,
-        },
-
+  {
+    eventId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        modelName: 'event',
-        sequelize: db
-    }
+    date: {
+      type: DataTypes.DATE,
+    },
+    location: {
+      type: DataTypes.STRING,
+    },
+    time: {
+      type: DataTypes.TIME
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    bands: {
+      type: DataTypes.STRING,
+    },
+    links: {
+      type: DataTypes.STRING,
+    },
+    isSoldOut: {
+      type: DataTypes.BOOLEAN,
+    },
+
+  },
+  {
+    modelName: 'event',
+    sequelize: db
+  }
 )
 
 class Order extends Model {
-    [util.inspect.custom]() {
-        return this.toJSON();
-    }
+  [util.inspect.custom]() {
+    return this.toJSON();
+  }
 }
 
 Order.init(
-    {
-        oderId: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        userId: {
-            type: DataTypes.INTEGER
-        },
-        productId: {
-            type: DataTypes.INTEGER,
-        },
+  {
+    oderId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        modelName: 'order',
-        sequelize: db
-    }
+    userId: {
+      type: DataTypes.INTEGER
+    },
+    productId: {
+      type: DataTypes.INTEGER,
+    },
+  },
+  {
+    modelName: 'order',
+    sequelize: db
+  }
 )
 
 class Like extends Model {
-    [util.inspect.custom]() {
-        return this.toJSON();
-    }
+  [util.inspect.custom]() {
+    return this.toJSON();
+  }
 }
 
 Like.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        userId: {
-            type: DataTypes.INTEGER,
-        },
-        bandId: {
-            type: DataTypes.INTEGER,
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        modelName: 'like',
-        sequelize: db
-    }
+    userId: {
+      type: DataTypes.INTEGER,
+    },
+    bandId: {
+      type: DataTypes.INTEGER,
+    },
+  },
+  {
+    modelName: 'like',
+    sequelize: db
+  }
 )
 
 if (process.argv[1] === url.fileURLToPath(import.meta.url)) {
-    console.log("Syncing to database...")
-    await db.sync()
-    console.log("Finished syncing database!")
+  console.log("Syncing to database...")
+  await db.sync()
+  console.log("Finished syncing database!")
 }
 
 //exports here
