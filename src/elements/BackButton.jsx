2835c6be98856e221d5a940c7944fb0b8a0ba9e0
Cat-Mills <141058467+Cat-Mills@users.noreply.github.com>
2023-10-24 12:14:12 -0600

    Finished cart

--- src/elements/BackButton.jsx
+++ src/elements/BackButton.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { ArrowLeft } from "../../icons";
 
 const BackButton = () => {
   const navigate = useNavigate();
@@ -8,9 +9,9 @@ const BackButton = () => {
     <div className="w-full h-[5vh] ml-4">
       <button
         onClick={() => navigate(-1)}
-        className="text-l font-semibold underline"
+        className="text-l font-semibold underline flex items-center"
       >
-        ← Go Back
+        <ArrowLeft/> Go Back
       </button>
     </div>
   );
